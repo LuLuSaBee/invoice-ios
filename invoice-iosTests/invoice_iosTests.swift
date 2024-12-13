4fc636449509cc5e523ab0cf7d93fcd8cb75adb3
Lewis Liu <lewisliu@synology.com>
2024-12-13 09:22:22 +0800

    First commit

---
+++ invoice-iosTests/invoice_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  invoice_iosTests.swift
+//  invoice-iosTests
+//
+//  Created by lewisliu on 2024/12/13.
+//
+
+import Testing
+@testable import invoice_ios
+
+struct invoice_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
