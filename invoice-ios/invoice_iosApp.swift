129a183bdd01e9032ba671c1c6852b1f4e412721
Lewis Liu <lewisliu@synology.com>
2024-12-13 17:38:42 +0800

    feat: Remove item model and add tabbar to content view

--- invoice-ios/invoice_iosApp.swift
+++ invoice-ios/invoice_iosApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct invoice_iosApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Invoice.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
