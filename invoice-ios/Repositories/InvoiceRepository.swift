2cb75ee575d510271457146797b94c7e9b9a3b4a
Lewis Liu <lulusabeetw@gmail.com>
2024-12-22 15:58:17 +0800

    feat: Update InvoiceRepository methods for fetching invoices by year and month

--- invoice-ios/Repositories/InvoiceRepository.swift
+++ invoice-ios/Repositories/InvoiceRepository.swift
@@ -5,29 +5,40 @@
 //  Created by 劉聖龍 on 2024/12/21.
 //
 
+import Foundation
 import SwiftData
 
 protocol InvoiceRepository {
-    func fetchInvoice(_ descriptor: FetchDescriptor<Invoice>) async throws -> [Invoice]
-    func insertInvoice(_ invoice: Invoice) async throws
-    func deleteInvoice(_ invoice: Invoice) async throws
+    func fetchInvoices(forYear year: Int, month: Int) async throws -> [Invoice]
+    func insertInvoice(_ invoice: Invoice) async
+    func deleteInvoice(_ invoice: Invoice) async
 }
 
 @ModelActor
-actor StandardInvoiceRepository: InvoiceRepository {
+actor SwiftDataInvoiceRepository: InvoiceRepository, Sendable {
     private var context: ModelContext { modelExecutor.modelContext }
 
-    func fetchInvoice(_ descriptor: FetchDescriptor<Invoice>) async throws -> [Invoice] {
-        try context.fetch(descriptor)
+    init(config: ModelConfiguration) {
+        let container = try! ModelContainer(for: Invoice.self, InvoiceDetail.self, configurations: config)
+        let modelContext = ModelContext(container)
+        self.modelExecutor = DefaultSerialModelExecutor(modelContext: modelContext)
+        self.modelContainer = container
     }
 
-    func insertInvoice(_ invoice: Invoice) async throws {
+    func fetchInvoices(forYear year: Int, month: Int) async throws -> [Invoice] {
+        let fetchDescriptor = FetchDescriptor<Invoice>(
+            predicate: #Predicate { $0.year == year && $0.month == month },
+            sortBy: [SortDescriptor(\.day, order: .reverse)]
+        )
+
+        return try context.fetch(fetchDescriptor)
+    }
+
+    func insertInvoice(_ invoice: Invoice) async {
         context.insert(invoice)
-        try context.save()
     }
 
-    func deleteInvoice(_ invoice: Invoice) async throws {
+    func deleteInvoice(_ invoice: Invoice) async {
         context.delete(invoice)
-        try context.save()
     }
 }
