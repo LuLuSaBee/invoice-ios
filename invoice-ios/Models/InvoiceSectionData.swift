548af4f731cf97b84fe34f1779ab5ba0b5563239
Lewis Liu <lewisliu@synology.com>
2024-12-23 16:28:25 +0800

    feat: Add invoice section data struct for invoice list

---
+++ invoice-ios/Models/InvoiceSectionData.swift
@@ -0,0 +1,12 @@
+//
+//  InvoiceSectionData.swift
+//  invoice-ios
+//
+//  Created by 劉聖龍 on 2024/12/22.
+//
+
+struct InvoiceSectionData {
+    var title: String
+    var totalAmount: Int
+    var invoices: [Invoice]
+}
