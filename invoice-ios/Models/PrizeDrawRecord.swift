894128a1631fc66f7eefc89a0746254e655ba533
Lewis Liu <lewisliu@synology.com>
2024-12-24 09:16:25 +0800

    fix: Fix normal struct can't be property of swift data model.

--- invoice-ios/Models/PrizeDrawRecord.swift
+++ invoice-ios/Models/PrizeDrawRecord.swift
@@ -10,16 +10,23 @@ import SwiftData
 
 @Model
 class PrizeDrawRecord {
-    #Unique<PrizeDrawRecord>([\.period])
-    #Index<PrizeDrawRecord>([\.period])
+    #Unique<PrizeDrawRecord>([\.year, \.firstMonth])
+    #Index<PrizeDrawRecord>([\.year, \.firstMonth])
 
-    private(set) var period: InvoicePeriod
+    private var year: Int
+    private var firstMonth: Int
+
     private(set) var specialNumber: String
     private(set) var grandNumber: String
     private(set) var firstNumbers: [String]
 
+    var period: InvoicePeriod {
+        InvoicePeriod(from: firstMonth, at: year)
+    }
+
     init(period: InvoicePeriod, specialNumber: String = "", grandNumber: String = "", firstNumbers: [String] = []) {
-        self.period = period
+        self.year = period.year
+        self.firstMonth = period.firstMonth
         self.specialNumber = specialNumber
         self.grandNumber = grandNumber
         self.firstNumbers = firstNumbers
