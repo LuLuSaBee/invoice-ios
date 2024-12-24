1515aab1a66078f91f916585720e2023c2215487
Lewis Liu <lewisliu@synology.com>
2024-12-24 18:03:46 +0800

    test: Add MockInvoiceRepository.

---
+++ invoice-iosTests/Mocks/MockInvoiceRepository.swift
@@ -0,0 +1,24 @@
+//
+//  MockInvoiceRepository.swift
+//  invoice-ios
+//
+//  Created by lewisliu on 2024/12/24.
+//
+
+@testable import invoice_ios
+
+class MockInvoiceRepository: InvoiceRepository {
+    private var invoices: [Invoice] = []
+
+    func fetchInvoices() async throws -> [Invoice] {
+        invoices
+    }
+
+    func insertInvoice(_ invoice: Invoice) async {
+        invoices.append(invoice)
+    }
+
+    func deleteInvoice(_ invoice: Invoice) async {
+        invoices.removeAll(where: { $0 == invoice })
+    }
+}
