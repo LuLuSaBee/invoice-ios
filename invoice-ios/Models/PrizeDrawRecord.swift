8da466fbc5c3daf16e20f9515ab5b8cd9832275f
Lewis Liu <lulusabeetw@gmail.com>
2024-12-26 23:13:05 +0800

    feat: Workaround for PrizeDrawRecord due to coredata not allow array of string.

--- invoice-ios/Models/PrizeDrawRecord.swift
+++ invoice-ios/Models/PrizeDrawRecord.swift
@@ -18,10 +18,16 @@ class PrizeDrawRecord {
 
     private(set) var specialNumber: String
     private(set) var grandNumber: String
-    private(set) var firstNumbers: [String]
+    private var firstNumber1: String = ""
+    private var firstNumber2: String = ""
+    private var firstNumber3: String = ""
 
     var period: InvoicePeriod {
         InvoicePeriod(from: firstMonth, at: year)
+    }
+
+    var firstNumbers: [String] {
+        [firstNumber1, firstNumber2, firstNumber3]
     }
 
     init(period: InvoicePeriod, specialNumber: String = "", grandNumber: String = "", firstNumbers: [String] = []) {
@@ -29,6 +35,17 @@ class PrizeDrawRecord {
         self.firstMonth = period.firstMonth
         self.specialNumber = specialNumber
         self.grandNumber = grandNumber
-        self.firstNumbers = firstNumbers
+
+        // WORKAROUND
+        // Error: CoreData: Could not materialize Objective-C class named "Array" from declared attribute value type "Array<String>"
+        if firstNumbers.indices.contains(0) {
+            self.firstNumber1 = firstNumbers[0]
+        }
+        if firstNumbers.indices.contains(1) {
+            self.firstNumber2 = firstNumbers[1]
+        }
+        if firstNumbers.indices.contains(2) {
+            self.firstNumber3 = firstNumbers[2]
+        }
     }
 }
